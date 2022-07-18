b9e2ed13b8e1ed42b86526897e20a592cb46f31f
Jarrod Merryman <jarrodmerryman@gmail.com>
2022-07-17 23:07:01 -0500

    adds github repo name

--- get-access-key.sh
+++ get-access-key.sh
@@ -2,4 +2,5 @@ RESOURCE_GROUP_NAME=$1
 STORAGE_ACCOUNT_NAME=$2
 
 ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query '[0].value' -o tsv)
+echo The Account Key: $ACCOUNT_KEY
 export ARM_ACCESS_KEY=$ACCOUNT_KEY
