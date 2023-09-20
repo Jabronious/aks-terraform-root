65c303be9f556e8669753152cd9161c679e135c6
Jarrod Merryman <jarrodmerryman@gmail.com>
2023-09-19 21:04:09 -0600

    update tfstate

--- terraform/aks-cluster.tf
+++ terraform/aks-cluster.tf
@@ -4,7 +4,7 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    storage_account_name = "tfstate29791"
+    storage_account_name = "tfstate10561"
     container_name       = "tfstate"
     key                  = "efit-aks-cluster.tfstate"
   }
